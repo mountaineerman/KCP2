7f64dfb579890027c4726e9754281a3c827fd7b0
Anton Deriabine <anton.deriabine@gmail.com>
2020-12-19 12:39:17 -0800

    Added KKIM high-level flow comments

--- KKIM/kkim-app/src/main/java/mountaineerman/kcp2/kkim/app/App.java
+++ KKIM/kkim-app/src/main/java/mountaineerman/kcp2/kkim/app/App.java
@@ -17,8 +17,15 @@ import krpc.client.services.KRPC;
 
 import static mountaineerman.kcp2.kkim.app.MessageUtils.getMessage;
 
-public class App {
-	public static void main(String[] args) {
+public class App
+{
+	public static void main(String[] args)
+	{
+		System.out.println("=======================================================================================");
+		System.out.println(" Kerbal Control Panel 2");
+		System.out.println("=======================================================================================");
+		
+		//TEMP:
 		System.out.println(">>>>> kkim-app: In main method");
 
 		LinkedList tokens;
@@ -26,14 +33,42 @@ public class App {
 		System.out.println(join(tokens));
 
 		System.out.println(KMegaApi.getHelloWorld());
+		
+		/* 	High-level flow:
+		 * 	
+		 * 	Initial:
+		 * 		-Establish connection to kRPC
+		 * 		-Establish connection to kMega
+		 * 		-Establish connection to kAndro
+		 * 	Loop:
+		 * 		-Confirm connection to kRPC
+		 * 		-Confirm connection to kMega
+		 * 		-Confirm connection between kMega and kNano 
+		 * 		-Confirm connection to kAndro
+		 *		 
+		 * 		A-Fetch latest kRPC values
+		 * 		B-Fetch latest kMega values
+		 * 		C-Update KIM object states based on A and B
+		 * 			-Send "packet" to kMega
+		 * 			-Send controlling signals to kRPC
+		 * 			-Send "packet" to kAndro
+		 * 
+		 * 		-Idle	
+		 */
 
-		try (Connection connection = Connection.newInstance()) {
+		try (Connection connection = Connection.newInstance())
+		{
 			KRPC krpc = KRPC.newInstance(connection);
 			System.out.println("Connected to kRPC version " + krpc.getStatus().getVersion());
-		} catch (RPCException e) {
+		}
+		catch (RPCException e)
+		{
 			System.out.println("Caught RPCException: " + e.getMessage());
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			System.out.println("Caught IOException: " + e.getMessage());
 		}
 	}
 }
+
