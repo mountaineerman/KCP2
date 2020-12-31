ee1d638387326d3399285c9bebf4af6adb17333f
Anton Deriabine <anton.deriabine@gmail.com>
2020-12-30 23:13:51 -0800

    StepperMotorTest: added assertTrue() statements for VALID edge case tests

--- KKIM/kkim-app/src/test/java/mountaineerman/kcp2/kkim/app/model/StepperMotorTest.java
+++ KKIM/kkim-app/src/test/java/mountaineerman/kcp2/kkim/app/model/StepperMotorTest.java
@@ -10,17 +10,19 @@ class StepperMotorTest {
 
 	@Test
 	void testSetDesiredPosition() {
-		
+
 		StepperMotor fakeMotor = new StepperMotor("fakeMotor", ModuleID.GT);
-		
+
 		// VALID
-		fakeMotor.setDesiredPosition(0);
-		//System.out.println(fakeMotor.getDesiredPosition());
-		fakeMotor.setDesiredPosition(3779);
-		//System.out.println(fakeMotor.getDesiredPosition());
-		fakeMotor.setDesiredPosition(1890);
-		//System.out.println(fakeMotor.getDesiredPosition());
-		
+		fakeMotor.setDesiredPosition(0); // Minimum boundary
+		Assertions.assertTrue(fakeMotor.getDesiredPosition() == 0);
+
+		fakeMotor.setDesiredPosition(3779); // Maximum boundary
+		Assertions.assertTrue(fakeMotor.getDesiredPosition() == 3779);
+
+		fakeMotor.setDesiredPosition(1890); // Midpoint
+		Assertions.assertTrue(fakeMotor.getDesiredPosition() == 1890);
+
 		// INVALID
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			fakeMotor.setDesiredPosition(-1); // Too low
