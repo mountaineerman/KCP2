b6d3bdfd60e33b8021dd3193594309f0ee5cbbb3
Anton Deriabine <anton.deriabine@gmail.com>
2020-12-30 22:51:52 -0800

    Renamed SwitchSP3T internal SP2T switches for clarity

--- KKIM/kkim-app/src/main/java/mountaineerman/kcp2/kkim/app/model/SwitchSP3T.java
+++ KKIM/kkim-app/src/main/java/mountaineerman/kcp2/kkim/app/model/SwitchSP3T.java
@@ -2,39 +2,39 @@ package mountaineerman.kcp2.kkim.app.model;
 
 public class SwitchSP3T extends Part {
 
-	private SwitchSP2T sp2tTopSwitch;
-	private SwitchSP2T sp2tBottomSwitch;
+	private SwitchSP2T topSensor;
+	private SwitchSP2T bottomSensor;
 	private SwitchSP3TPosition position;
 	
 	public SwitchSP3T(String name, ModuleID moduleID, SwitchSP2T top_switch, SwitchSP2T bottom_switch) {
 		super(name, moduleID);
-		this.sp2tTopSwitch = top_switch; //TODO confirm format
-		this.sp2tBottomSwitch = bottom_switch; //TODO confirm format
+		this.topSensor = top_switch; //TODO confirm format
+		this.bottomSensor = bottom_switch; //TODO confirm format
 	}
 
 	public SwitchSP3TPosition getPosition() {
 		return position;
 	}
 
-	public void setTopSwitchStatus(boolean status) {
-		this.sp2tTopSwitch.setStatus(status);
+	public void setTopSensorStatus(boolean status) {
+		this.topSensor.setStatus(status);
 	}
 	
-	public void setBottomSwitchStatus(boolean status) {
-		this.sp2tBottomSwitch.setStatus(status);
+	public void setBottomSensorStatus(boolean status) {
+		this.bottomSensor.setStatus(status);
 	}
 
 	public void updatePosition() {
-		if(this.sp2tTopSwitch.getStatus() == true) {
-			if(this.sp2tBottomSwitch.getStatus() == true) {
+		if(this.topSensor.getStatus() == true) {
+			if(this.bottomSensor.getStatus() == true) {
 				this.position = SwitchSP3TPosition.INVALID;
-				String message = String.format("%s is in an invalid position. Top and Bottom SP2T switches are both ON.", this.name);
+				String message = String.format("%s is in an invalid position. Top and Bottom sensors are both ON.", this.name);
 				throw new IllegalArgumentException(message); //TODO Replace with better exception type; Handle exception gracefully...
 			} else {
 				this.position = SwitchSP3TPosition.TOP;
 			}
 		} else {
-			if(this.sp2tBottomSwitch.getStatus() == true) {
+			if(this.bottomSensor.getStatus() == true) {
 				this.position = SwitchSP3TPosition.BOTTOM;
 			} else {
 				this.position = SwitchSP3TPosition.CENTER;
--- KKIM/kkim-app/src/test/java/mountaineerman/kcp2/kkim/app/model/SwitchSP3TTest.java
+++ KKIM/kkim-app/src/test/java/mountaineerman/kcp2/kkim/app/model/SwitchSP3TTest.java
@@ -15,25 +15,25 @@ class SwitchSP3TTest {
 		SwitchSP3T testSP3TSwitch = new SwitchSP3T("testSP3TSwitch", ModuleID.A, topSwitch, bottomSwitch);
 		
 		// VALID
-		testSP3TSwitch.setTopSwitchStatus(true);
-		testSP3TSwitch.setBottomSwitchStatus(false);
+		testSP3TSwitch.setTopSensorStatus(true);
+		testSP3TSwitch.setBottomSensorStatus(false);
 		testSP3TSwitch.updatePosition();
 		Assertions.assertTrue(testSP3TSwitch.getPosition() == SwitchSP3TPosition.TOP);
 		
-		testSP3TSwitch.setTopSwitchStatus(false);
-		testSP3TSwitch.setBottomSwitchStatus(false);
+		testSP3TSwitch.setTopSensorStatus(false);
+		testSP3TSwitch.setBottomSensorStatus(false);
 		testSP3TSwitch.updatePosition();
 		Assertions.assertTrue(testSP3TSwitch.getPosition() == SwitchSP3TPosition.CENTER);
 		
-		testSP3TSwitch.setTopSwitchStatus(false);
-		testSP3TSwitch.setBottomSwitchStatus(true);
+		testSP3TSwitch.setTopSensorStatus(false);
+		testSP3TSwitch.setBottomSensorStatus(true);
 		testSP3TSwitch.updatePosition();
 		Assertions.assertTrue(testSP3TSwitch.getPosition() == SwitchSP3TPosition.BOTTOM);
 		
 		// INVALID
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			testSP3TSwitch.setTopSwitchStatus(true);
-			testSP3TSwitch.setBottomSwitchStatus(true);
+			testSP3TSwitch.setTopSensorStatus(true);
+			testSP3TSwitch.setBottomSensorStatus(true);
 			testSP3TSwitch.updatePosition();
 		});
 		Assertions.assertTrue(testSP3TSwitch.getPosition() == SwitchSP3TPosition.INVALID);
