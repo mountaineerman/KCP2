875e7c95225b00eb18f9ce7037b0ac64c67d29c5
Anton Deriabine <anton.deriabine@gmail.com>
2021-12-23 17:34:50 -0700

    Unknown changes that were not committed last time

--- KMega/KMega.cpp
+++ KMega/KMega.cpp
@@ -20,33 +20,44 @@
 
 
 
-//INITIALIZE PARTS
-MuxShield mux; //TODO replace with other constructor call
-
+//INITIALIZE COMMON PARTS
+MuxShield mux;
 Adafruit_TLC5947 ledDriverBoards(NUMBER_OF_LED_DRIVER_BOARDS, PIN_LED_DRIVER_BOARDS_CLOCK, PIN_LED_DRIVER_BOARDS_DATA_IN, PIN_LED_DRIVER_BOARDS_LATCH);
-
-//TODO Initialize remaining parts...
-
-
-
 
 void setup() {
 
-	mux.setMode(MULTIPLEXER_IO_ROW_1,DIGITAL_IN_PULLUP);
-	mux.setMode(MULTIPLEXER_IO_ROW_2,DIGITAL_IN);
-	mux.setMode(MULTIPLEXER_IO_ROW_3,DIGITAL_IN);
+	mux.setMode(MULTIPLEXER_IO_ROW_1,DIGITAL_IN_PULLUP); //TODO is this right?
+	mux.setMode(MULTIPLEXER_IO_ROW_2,DIGITAL_IN);		 //TODO is this right?
+	mux.setMode(MULTIPLEXER_IO_ROW_3,DIGITAL_IN);		 //TODO is this right?
 
 	ledDriverBoards.begin();
 	//TODO replace the following 2 lines with an LED OVERRIDE function... And add a bit to kkim>kmega interface...
 	pinMode(PIN_LED_DRIVER_BOARDS_OVERRIDE, OUTPUT);
-	digitalWrite(PIN_LED_DRIVER_BOARDS_OVERRIDE, LOW);
+	digitalWrite(PIN_LED_DRIVER_BOARDS_OVERRIDE, HIGH);//LOW);
 
 	Serial.begin(COMPUTER_BAUD_RATE);
 	Serial.println("Hello World! (from Arduino Mega)");
+	delay(3000);
+
 }
 
-void loop() {
+//TODO Initialize remaining parts...
+//Module F
+//SwitchSP2T switch_trim(3, false, 3, mux);
 
+
+int n = 0;
+void loop() {
+	//switch_trim.refreshStatus();
+
+	Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();
+	Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();
+	Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();Serial.println();
+	Serial.println("MODULE F");
+	Serial.print("TRIM: "); Serial.println(mux.digitalReadMS(3,3));//n++switch_trim.getStatus());
+
+
+	delay(1000);
 }
 
 /* ==============================================================================================
