3cb649b57ecd7c0942c114de88b460fd71a26044
Anton Deriabine <anton.deriabine@gmail.com>
2021-12-27 12:48:25 -0700

    Moved "LED" comments out of KMega.cpp and into an inert header, "LED.h"

--- KMega/KMega.cpp
+++ KMega/KMega.cpp
@@ -60,23 +60,4 @@ void loop() {
 	delay(1000);
 }
 
-/* ==============================================================================================
- * OUTPUTS
- * ==============================================================================================
- * LED
- * (Written exclusively for being driven by Arduino, because of design constraints)
- *
- * 		pinNumber (range: Nano: Theoretical: 0-12, A0-A7	)
- * 				  (		  Nano: By design:   A1-A4			)
- * 				  (		  Mega: Theoretical: 0-53, A0-A15	)
- * 				  (		  Mega: By design:   None			)
- * 				  (		  Mux:  By design:   None			)
- *
- * 		bool status
- * 				TRUE:  ON
- * 				FALSE: OFF
- *
- * 		setStatus(bool)
- *
- * ArduinoNano
- */
+
