6eb78ced01cc19fdb9fc661d8018a2fc90490140
Anton Deriabine <anton.deriabine@gmail.com>
2022-01-19 21:15:10 -0700

    Added a Common Utilities header, containing a function to clear the Serial Monitor screen.

---
+++ KMega/libraries/CommonUtilities/CommonUtilities.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_UTILITIES_h
+#define COMMON_UTILITIES_h
+
+#include <Arduino.h>
+
+
+void clearScreen() {
+	
+	Serial.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	
+}
+
+
+#endif
