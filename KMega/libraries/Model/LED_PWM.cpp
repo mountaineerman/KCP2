b9f2ae9f545749ec4ef6bc935535d7c3f227bb64
Anton Deriabine <anton.deriabine@gmail.com>
2022-03-19 18:02:13 -0600

    (PARTIAL: 3/3) Updated LED_PWM to (1) use int instead of uint16_t, and (2) cap the setPWM value at PWM_LED_MAXIMUM if it is greater.

--- KMega/libraries/Model/LED_PWM.cpp
+++ KMega/libraries/Model/LED_PWM.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <LED_PWM.h>
 #include <Adafruit_TLC5947.h>
+#include "../../configuration.h"
 
 LED_PWM::LED_PWM(uint16_t channel, Adafruit_TLC5947& ledDriverBoards)
 	: ledDriverBoards(ledDriverBoards)
@@ -9,13 +10,13 @@ LED_PWM::LED_PWM(uint16_t channel, Adafruit_TLC5947& ledDriverBoards)
 	this->pwm = 0;
 }
 
-void LED_PWM::setPWM(uint16_t pwm) {
+void LED_PWM::setPWM(int pwm) {
+	
+	if (pwm > PWM_LED_MAXIMUM) {
+		pwm = PWM_LED_MAXIMUM;
+	}
+	
 	this->pwm = pwm;
 	this->ledDriverBoards.setPWM(this->channel, this->pwm);
 	this->ledDriverBoards.write(); //TODO replace this with one call every refresh cycle
-}
-
-//Used for debugging only:
-uint16_t LED_PWM::getPWM() {
-	return this->pwm;
 }
--- KMega/libraries/Model/LED_PWM.h
+++ KMega/libraries/Model/LED_PWM.h
@@ -10,16 +10,12 @@
  * However, for KCP2, the hardware design is such that all PWM LEDs are driven by the
  * TLC5947 LED Driver Chip (http://www.adafruit.com/products/1429). This class is written
  * with this limitation in mind.
- *
- * No input validation is performed. */
+ */
 class LED_PWM
 {
 public:
 	LED_PWM(uint16_t channel, Adafruit_TLC5947& ledDriverBoards);
-	void setPWM(uint16_t pwm);
-protected:
-	//Used for debugging only:
-	uint16_t getPWM();
+	void setPWM(int pwm);
 private:
 	/* Range: Board1: 0-23,
 	 * 		  Board2: 24-47,
@@ -29,7 +25,7 @@ private:
 	/* The PWM value sent to the TLC5947 LED Driver Chip. Range: [0-4095]. For more information, see:
 	 *     -Adafruit_TLC5947.h
 	 *     -http://www.adafruit.com/products/1429 */
-	uint16_t pwm;
+	int pwm;
 	Adafruit_TLC5947& ledDriverBoards;
 };
 
