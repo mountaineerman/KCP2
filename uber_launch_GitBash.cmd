be1c96e44eed639ba4e31f8797db646a688472c4
Anton Deriabine <anton.deriabine@gmail.com>
2024-08-12 12:37:40 -0600

    Updated launch cmd scripts for convenience; fixed git bash launch command (problem with git switch).

---
+++ uber_launch_GitBash.cmd
@@ -0,0 +1,2 @@
+start "" "%ProgramFiles%\Git\bin\sh.exe" ^
+-c "cd /c/dev/KCP2 && git status && echo ' ' && echo 'REMEMBER: switch to the appropriate branch...' && /usr/bin/bash --login -i"
---
+++ uber_launch_KSP.cmd
@@ -0,0 +1,2 @@
+cd "C:\Users\anton\Documents\Kerbal Space Program\_KSP_CKAN_1.5.1_v3\KSP_win64"
+start "KSP" KSP_x64.exe
