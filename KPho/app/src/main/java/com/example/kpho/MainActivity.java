863a59bf3b11f1ffa91d85fe6d50861ec41acb9e
Anton Deriabine <anton.deriabine@gmail.com>
2025-02-17 08:14:54 -0700

    KPho: Added basic layout

--- KPho/app/src/main/java/com/example/kpho/MainActivity.java
+++ KPho/app/src/main/java/com/example/kpho/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.kpho;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button startServerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +26,57 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+//        // Hide the status+navigation bars
+//        View decorView = getWindow().getDecorView();
+//        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+//        decorView.setSystemUiVisibility(uiOptions);
+//
+//        // Hide the action bar
+//        ActionBar actionBar = getActionBar();
+//        if (actionBar != null) {
+//            actionBar.hide();
+//        }
+
+        startServerButton = (Button) findViewById(R.id.StartServerButton);
+        // When the button is pressed
+        startServerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                //TODO: prevent tap on screen from exiting fullscreen mode
+
+                // Hide the status+navigation bars
+                View decorView = getWindow().getDecorView();
+                int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+                decorView.setSystemUiVisibility(uiOptions);
+
+                // Hide the action bar
+                ActionBar actionBar = getActionBar();
+                if (actionBar != null) {
+                    actionBar.hide();
+                }
+
+                Log.i("KPho", "User tapped the Start Server Button...");
+
+            }
+        });
+    }
+
+    //Necessary to hide the (top) status/navigation/action bar if switching back to app
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        // Hide the status bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        //int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        // Hide the action bar
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
     }
 }
