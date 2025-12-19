5b49338b3366753f80717b84404069990e897c7d
Anton Deriabine <anton.deriabine@gmail.com>
2025-12-19 10:09:26 -0700

    NGH: Changed programming notes for Linux and made them specific to NGH_A

--- NGH/NGH.ino
+++ NGH/NGH.ino
@@ -4,10 +4,11 @@
 #include <NGHService.h>
 
 /* REMINDER: 
-When programming KNano, set:
+When programming NGH_A, set:
 	1. File > Preferences > Sketchbook location = /home/anton/KerbalSpaceProgram/KCP2/NGH
 	2. Tools > Board = Arduino megaAVR Boards > Arduino Nano Every
-	3. Tools > Port = /dev/ttyACM0 (NGH A) / COM5 (NGH B)
+	3. Tools > Port = /dev/ttyACM0
+	4. Tools > Registers Emulation = None (ATMEGA4809)
 
 To switch between GaugePacketA and GaugePacketB search for: "[GaugePacketA]" / "[GaugePacketB]"
 */
