05ef759cf11756469b74d7a539d40d74816741bb
Anton Deriabine <anton.deriabine@gmail.com>
2025-12-21 20:54:03 -0700

    KMega: (UNTESTED BUT COMPILES) updated notes for linux; removed some of the unused StepperMotor2 methods.

--- KMega/KMega.ino
+++ KMega/KMega.ino
@@ -5,10 +5,10 @@
 
 /* REMINDER: 
 When programming KMega, set:
-	1. File > Preferences > Sketchbook location = C:\dev\KCP2\KMega
-	2. Tools > Board = Arduino Mega or Mega 2560
+	1. File > Preferences > Sketchbook location = /home/anton/KerbalSpaceProgram/KCP2/KMega
+	2. Tools > Board = Arduino AVR Boards > Arduino Mega or Mega 2560
 	3. Tools > Processor = ATmega2560 (Mega 2560)
-	4. Tools > Port = COM3 << If this changes, need to update:
+	4. Tools > Port = /dev/ttyACM0 << If this changes, need to update:
 		C:\dev\KCP2\KMega\configuration.h: COMMUNICATION_PORT
 		C:\dev\KCP2\KKIM\config.properties: kmega.port.number
 */
