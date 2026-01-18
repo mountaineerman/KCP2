630d26ef4c9c9d036653230bc03572fa57517637
Anton Deriabine <anton.deriabine@gmail.com>
2026-01-18 10:45:56 -0700

    LaunchScripts: update settings files for dev/play based on KSP key bindings and graphics settings.

--- uber_launch_KCP2_dev.sh
+++ uber_launch_KCP2_dev.sh
@@ -11,8 +11,8 @@ echo "==========================================================================
 echo "KSP"
 echo "---------------------------------------------------------------------------------------------------------------"
 cd /home/anton/KerbalSpaceProgram/KSP_GameArchive/KSP_linux
-#echo "Overwriting settings.cfg file and launching KSP..."
-#cp -p /home/anton/KerbalSpaceProgram/KCP2/LaunchScripts/settings.cfg.dev settings.cfg
+echo "Overwriting settings.cfg file and launching KSP..."
+cp -p /home/anton/KerbalSpaceProgram/KCP2/LaunchScripts/settings.cfg.dev settings.cfg
 TERM=xterm ./KSP.x86_64 > ksp_script_launch_output.txt &
 echo " "
 
--- uber_launch_KCP2_play.sh
+++ uber_launch_KCP2_play.sh
@@ -1,7 +1,7 @@
 #KSP
 cd /home/anton/KerbalSpaceProgram/KSP_GameArchive/KSP_linux
-#echo "Overwriting settings.cfg file and launching KSP..."
-#cp -p /home/anton/KerbalSpaceProgram/KCP2/LaunchScripts/settings.cfg.play settings.cfg
+echo "Overwriting settings.cfg file and launching KSP..."
+cp -p /home/anton/KerbalSpaceProgram/KCP2/LaunchScripts/settings.cfg.play settings.cfg
 TERM=xterm ./KSP.x86_64 > ksp_script_launch_output.txt &
 
 #KKIM (via JAR)
